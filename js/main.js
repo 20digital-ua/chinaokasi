ba3572f7d61ef10300a63a0941692ae4b4e115a2
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-04-24 10:26:34 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-jQuery(document).ready(function () {
+class Test {
+    constructor(){
+        this.printResult=jQuery('.result');
+        this.btn=jQuery('.doit');
+        // this.lap();
+        this.events();
+    }
+    // Events
+    events(){
+        //this.btn.hover(this.lap.bind(this));
+
+    }
 
 
 
-});
+    // Methods
+    lap(){
+        //this.printResult.append('A ');
+    }
+
+
+
+}
 
 
 
 
-
+var item = new Test();
