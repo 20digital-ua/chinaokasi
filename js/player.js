429ad0a4b7c1659048991cbd4f3c3125da865b41
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-04-26 13:56:49 +0300

    podcast player

---
+++ js/player.js
@@ -0,0 +1,36 @@
+
+var player=jQuery('#podcast-player-tag')[0];
+var playBtn=jQuery('#podcast-playBtn');
+var progress=jQuery('#progress-bar');
+var progressFill=jQuery('#progress-bar__fill');
+
+// Events
+playBtn.click(togglePlay);
+player.addEventListener("timeupdate", updateProgress);
+
+
+
+// Methods
+function togglePlay(){
+    if (player.paused) {
+        player.play();
+        playBtn.addClass('playing');
+    } else {
+        player.pause();
+        playBtn.removeClass('playing');
+    }
+}
+function updateProgress(){
+    var currentTime = player.currentTime;
+    var percent = (100/player.duration)*currentTime;
+
+    progressFill.css({
+        'width':percent+'%'
+    });
+}
+
+
+
+
+
+
