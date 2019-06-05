0e66bbb4dcc2e47590314341c9b867744aec1ff4
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-06-05 10:09:40 +0300

    TV Page

--- js/tabs.js
+++ js/tabs.js
@@ -1,14 +1,17 @@
 $(".tab-body").hide();
     $(".tab-body:first").show();
     $(".tab-slider--nav li").click(function() {
-        $(".tab-body").hide();
-        var activeTab = $(this).attr("rel");
-        $("#"+activeTab).fadeIn();
-        if($(this).attr("rel") == "tab2"){
-            $('.tab-slider--tabs').addClass('slide');
-        }else{
-            $('.tab-slider--tabs').removeClass('slide');
+        if($(this).hasClass('active')!=true){
+            $(".tab-body").hide();
+        
+            var activeTab = $(this).attr("rel");
+            $("#"+activeTab).fadeIn();
+            if($(this).attr("rel") == "tab2"){
+                $('.tab-slider--tabs').addClass('slide');
+            }else{
+                $('.tab-slider--tabs').removeClass('slide');
+            }
+            $(".tab-slider--nav li").removeClass("active");
+            $(this).addClass("active");
         }
-        $(".tab-slider--nav li").removeClass("active");
-        $(this).addClass("active");
     });
