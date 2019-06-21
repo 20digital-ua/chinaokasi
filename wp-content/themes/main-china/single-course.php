f59c6786388ef7b941b1d18cace6420259b044d8
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-06-21 12:07:03 +0300

    Add General Podcast

--- wp-content/themes/main-china/single-course.php
+++ wp-content/themes/main-china/single-course.php
@@ -50,24 +50,47 @@ while(have_posts()){
 						</div>
 						
 					</div>
+					<?php
+			
+			
+					$getPodcasts= new WP_Query(array(
+						'posts_per_page'=>-1,
+						'post_type'=>'podcast',
+						'orderby' => 'publish_date', 
+						'order' => 'DESC'
+
+					));
+					// var_dump($getPodcasts);
+					while($getPodcasts->have_posts()){
+						$getPodcasts->the_post();
+						$isGeneral=get_field('general_podcast');
+						if($isGeneral[0]=='true'){
+							$podcastSrc=get_field('media-data');
+							$titleText=get_the_content();
+							break;
+						}else{
+							$podcastSrc=get_field('media-data');
+							$titleText=get_the_content();
+						}
+					}
+					?>
 					<div class="podcast single-post  grid-padding">
 						<h2 class="title title--left">Heard My Hot Podcast?</h2>
 						<h2 class="podcast-intro-text">China & Friends</h2>
-						<div class="container container--podcast-player"></div>
+						<div class="container container--podcast-player">
 							<div class="podcast-player">
 								<div class="podcast-info">
-										<p class="podcast-player__title"><span class="inner-bold">China and Friends</span> Podcast</p>
-										<p class="podcast-player__episode">Episode #1</p>
+										<p class="podcast-player__title"><?=$titleText?></p>
 								</div>
 								<div class="podcast-controls">
-									<button id="podcast-playBtn"></button>
-									<div id="progress-bar">
-										<div id="progress-bar__fill">
+									<button  id="podcast-1" class="podcast-playBtn"></button>
+									<div class="progress-bar" id="podcast-1__bar">
+										<div class="progress-bar__fill" id="podcast-1__audio__bar-fill">
 											<div class="progress-bar__pin">	</div>
 										</div>
 									</div>
 								</div>
-								<audio id="podcast-player-tag" src="<?php echo get_template_directory_uri(); ?>/source/audio/kazka-plakala-rington_(muztron.com).mp3"></audio>
+								<audio id="podcast-1__audio" class="podcast-player-tag" ontimeupdate="updateProgress(this)" src="<?=$podcastSrc?>"></audio>
 								<!--<audio id="podcast-player-tag" src="source/audio/Ariana Grande - 7 rings-320.mp3"></audio>-->
 							</div>
 							<div class="sign-podcast">
